bb74faca313f66078de4d49aa9fe5b65bf448668
Christina Kirk <cmariekirk@Christinas-MBP.hsd1.ma.comcast.net>
2018-04-16 18:31:44 -0400

    Trying to live moniter new posts on r/coffee/new code currently not working

--- Redditbotv1.py
+++ Redditbotv1.py
@@ -25,11 +25,12 @@ reddit = praw.Reddit(username = config.username,
 conn = psycopg2.connect(dbname='redditbot', user='postgres')
 cur = conn.cursor()
 
-"""Automated bot that checks reddit.com/r/new every ten seconds and
-adds the title and url of each posts to a postgres database"""
+
 
 
 def store_new_posts():
+    """Automated bot that checks reddit.com/r/all/new every ten seconds and
+    adds the title and url of each posts to a postgres database"""
     counter = 1
     while True: 
         for submission in reddit.subreddit('all').new(limit=10):
@@ -42,10 +43,13 @@ def store_new_posts():
         time.sleep(30)
 
 def find_coffee_posts():
+    """Automated bot that checks reddit.com/r/all/new every ten seconds for posts whose
+    title's contain the world 'coffee' and sends the date, title, and url
+    of each post to a postgres database"""
     while True:
         for submission in reddit.subreddit('all').new(limit=10):
             x=submission.title.lower()
-            if x.find('the') != -1:
+            if x.find('coffee') != -1:
                 print("Found Coffee Post!")
                 remove_char=submission.title.replace("'","")    
                 sql_command = "INSERT INTO coffee_posts (date,title,url) VALUES ('%s','%s','%s');" % (datetime.datetime.now(),remove_char,submission.url)
@@ -53,6 +57,22 @@ def find_coffee_posts():
                 conn.commit()
             else:
                 print("No posts found")
-        time.sleep(60*5)
+        time.sleep(30)
 
-find_coffee_posts()
+def rcoffee_collection():
+    while True:
+        for submission in reddit.subreddit('coffee').new(limit=1):
+            last_post = ''
+            if submission.url != last_post:
+                remove_char=submission.title.replace("'","")    
+                sql_command = "INSERT INTO rcoffee_posts (date,title,url) VALUES ('%s','%s','%s');" % (datetime.datetime.now(),remove_char,submission.url)
+                cur.execute(sql_command)
+                conn.commit()
+                last_post = submission.url
+                print('adding post to database')
+            else:
+                print('Repeated Post. Sleeping for 3 seconds')
+            time.sleep(3)
+
+
+rcoffee_collection()
